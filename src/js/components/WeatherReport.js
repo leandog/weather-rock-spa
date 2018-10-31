61463d168fd25b219152c18bde59685470de296a
Dan Parks <dan.parks@leandogsoftware.com>
2018-10-31 09:10:41 -0600

    make weather report responsive; refactoring

--- src/js/components/WeatherReport.js
+++ src/js/components/WeatherReport.js
@@ -11,9 +11,24 @@ class WeatherReport extends React.Component {
   render() {
     // TODO: include name input and answer history
     return (
-      <div className={`forecast ${this.props.forecast}`}>
-        <aside>Your official Weather Rock forecast</aside>
-        <p>{kb[this.props.forecast].label}</p>
+      <div className="weather-report">
+        <h2>Your official Weather Rock forecast</h2>
+        <div className={`forecast ${this.props.forecast}`}>
+          <aside>Today</aside>
+          <p>{kb[this.props.forecast].label}</p>
+        </div>
+        <div className="forecast">
+          <aside>Tomorrow</aside>
+          <p>Who knows?</p>
+        </div>
+        <div className="forecast">
+          <aside>In 2 Days</aside>
+          <p>The Rock...</p>
+        </div>
+        <div className="forecast">
+          <aside>In 3 Days</aside>
+          <p>Knows!</p>
+        </div>
       </div>
     );
   }
--- src/js/components/inputs.js
+++ src/js/components/inputs.js
@@ -2,32 +2,26 @@ import React from "react";
 
 export const TextInput = ({ label }) => (
   <div>
-    <label>
-      {label}
-      <input type="text" />
-    </label>
+    <label>{label}</label>
+    <input type="text" />
   </div>
 );
 
 export const SelectInput = ({ label, options }) => (
   <div>
-    <label>
-      {label}
-      <select>
-        {options.map((option, index) => {
-          return <option key={index} value={option.value}>{option.label}</option>;
-        })}
-      </select>
-    </label>
+    <label>{label}</label>
+    <select>
+      {options.map((option, index) => {
+        return <option key={index} value={option.value}>{option.label}</option>;
+      })}
+    </select>
   </div>
 );
 
 export const YesNoInput = ({ label }) => (
   <div>
-    <label>
-      {label}:
-      Yes <input type="radio" name="question" value="yes" />
-      No <input type="radio" name="question" value="no" />
-    </label>
+    <p>{label}</p>
+    <label><input type="radio" name="question" value="yes" /> Yes</label>
+    <label><input type="radio" name="question" value="no" /> No</label>
   </div>
 );
